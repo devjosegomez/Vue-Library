a386d050147e6eea87cec1084acb1c082c8f26e4
devjosegomez <devjosegomez@gmail.com>
2020-03-13 12:11:10 -0600

    responsive-img @ home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="row card-content valign center">
     <div class="row">
-      <div class="card-image">
-        <img src="https://i.imgur.com/t52SVPE.jpg">
+      <div class="card-image responsive-img">
+        <img src="https://i.imgur.com/t52SVPE.jpg" class="responsive-img">
       </div>
       <div class="card-content">
         <span class="card-title activator grey-text text-darken-4"><h3>Welcome to Hey! Library</h3><i class="material-icons right">more_vert</i></span>
